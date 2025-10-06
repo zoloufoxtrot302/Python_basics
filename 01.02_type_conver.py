4325a15a3d90648812669b3fa1cbdeb803ba859e
zoloufoxtrot302 <117604147+zoloufoxtrot302@users.noreply.github.com>
2025-10-06 14:53:13 +0500

    Add files via upload

---
+++ 01.02_type_conver.py
@@ -0,0 +1,36 @@
+
+print("PRINTING A W3SCHOOL CODE")
+x = 1    # int
+y = 2.8999  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c)) 
+
+print("MY PRACTICE STARTS FROM HERE")
+
+x1 = 10
+y1 = 5.55
+z1 = 5j
+
+a1 = float(x1)
+b1 = int(y1)
+c1 = complex(x1)
+
+print(a1)
+print(b1)
+print(c1)
