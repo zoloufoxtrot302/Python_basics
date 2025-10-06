ab205518982ccf7e13c52ea31ad1a1bd21843c0d
zoloufoxtrot302 <117604147+zoloufoxtrot302@users.noreply.github.com>
2025-10-06 14:52:30 +0500

    Add files via upload

---
+++ 01.03_Datatypes.py
@@ -0,0 +1,19 @@
+
+x = str("s1") #x will be be 's1'
+
+y = str(2)
+
+z = str(3.0)
+
+def myfunc():
+    return v1+v2
+v1= str(2.0)
+v2= str(3.0)
+
+res = myfunc(v1,v2)
+
+print(myfunc(res))
+
+print(x)
+print(y)
+print(z)
